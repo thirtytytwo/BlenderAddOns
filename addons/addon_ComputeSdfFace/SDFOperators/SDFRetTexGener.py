cfe4518cb3201b931dd9cd8c41a1aeebd51a7e8a
luyicheng <thirtytwo0301@gmail.com>
2024-12-31 17:27:18 +0800

    更新

--- addons/addon_ComputeSdfFace/SDFOperators/SDFRetTexGener.py
+++ addons/addon_ComputeSdfFace/SDFOperators/SDFRetTexGener.py
@@ -1,18 +1,19 @@
 import concurrent.futures
+import math
 import numpy as np
 import bpy
+import copy
 
 class Point:
+    
     def __init__(self, dx, dy):
-        self.dx = dx
-        self.dy = dy
+        self.dx = int(dx)
+        self.dy = int(dy)
     
     def GetDistance(self):
-        return self.dx * self.dx + self.dy * self.dy
+        return int(self.dx * self.dx + self.dy * self.dy)
     
-def ComputeSDF(image):
-        width = image.size[0]
-        height = image.size[1]
+def ComputeSDF(data, width, height):
         res = max(width, height)
         
         grid1 = [[Point(0, 0) for x in range(width)] for y in range(height)]
@@ -20,38 +21,38 @@ def ComputeSDF(image):
         
         for y in range(height):
             for x in range(width):
-                if image.pixels[(y * width + x) * 4] < 0.5:
+                if data[(y * width + x)] < 0.5:
                     grid1[y][x] = Point(0, 0)
                     grid2[y][x] = Point(res, res)
                 else:
                     grid1[y][x] = Point(res, res)
                     grid2[y][x] = Point(0, 0)
+
         #TODO 这里没有依赖关系，可以用两个线程做
         with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
-            future1 = executor.submit(ComputeGrid, grid1, height, width)
-            future2 = executor.submit(ComputeGrid, grid2, height, width)
-            future1.result()
-            future2.result()
+            futures = []
+            futures.append(executor.submit(ComputeGrid, grid1, height, width))
+            futures.append(executor.submit(ComputeGrid, grid2, height, width))
+            
+            for future in concurrent.futures.as_completed(futures):
+                future.result()
 
         #TODO 这里没有依赖关系，可以用多线程
-        retImage = bpy.data.images.new(name = image.name + "_sdf", width = width, height = height)
+        ret = []
         for y in range(height):
             for x in range(width):
-                with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
-                    futures = []
-                    for y in range(height):
-                        for x in range(width):
-                            futures.append(executor.submit(ComputePixel, grid1, grid2, retImage, x, y, width))
-                    for future in futures:
-                        future.result()
-        retImage.update()
+                ret.append(ComputePixel(grid1, grid2, x, y))
+        return ret
 
-def ComputePixel(grid1, grid2, image, x, y, width):
-    d1 = grid1[y][x].GetDistance()
-    d2 = grid2[y][x].GetDistance()
-    val = (d1 - d2) / (d1 + d2)
-    normalized_value = (val + 1) / 2
-    image.pixels[(y * width + x) * 4 : (y * width + x) * 4 + 4] = [normalized_value] * 4
+def ComputePixel(grid1, grid2, x, y):
+    d1 = int(math.sqrt(float(Get(grid1, x, y).GetDistance())))
+    d2 = int(math.sqrt(float(Get(grid2, x, y).GetDistance())))
+    dist = d1 - d2
+    val = dist * 2 + 128
+    if val < 0: val = 0
+    if val > 255: val = 255
+    
+    return val
         
 def Get(grid, x, y):
     if x >= 0 and y >=0 and x < len(grid[0]) and y < len(grid):
@@ -60,12 +61,14 @@ def Get(grid, x, y):
         return Point(len(grid), len(grid))
         
 def Compare(grid, point, x, y, offsetX, offsetY):
-    otherP = Get(grid, x + offsetX, y + offsetY)
-    otherP.dx += offsetX
-    otherP.dy += offsetY
-    
-    if otherP.GetDistance() < point.GetDistance():
-        point = otherP
+    temp = copy.copy(Get(grid, x + offsetX, y + offsetY))
+    temp.dx += offsetX
+    temp.dy += offsetY
+
+    if temp.GetDistance() < point.GetDistance():
+        point.dx = temp.dx
+        point.dy = temp.dy
+
 
 def ComputeGrid(grid, height, width):
         for y in range(height):
@@ -76,11 +79,9 @@ def ComputeGrid(grid, height, width):
                 Compare(grid, point, x, y, 0, -1)
                 Compare(grid, point, x, y, -1, -1)
                 Compare(grid, point, x, y, 1, -1)
-                grid[y][x] = point
             for x in range(width - 1, -1, -1):
                 point = Get(grid, x, y)
                 Compare(grid, point, x, y, 1, 0)
-                grid[y][x] = point
                 
         for y in range(height - 1, -1, -1):
             for x in range(width - 1, -1, -1):
@@ -90,11 +91,9 @@ def ComputeGrid(grid, height, width):
                 Compare(grid, point, x, y, 0, 1)
                 Compare(grid, point, x, y, -1, 1)
                 Compare(grid, point, x, y, 1, 1)
-                grid[y][x] = point
             for x in range(width):
                 point = Get(grid, x, y)
                 Compare(grid, point, x, y, -1, 0)
-                grid[y][x] = point
 
 class SDFRetTexGenOperator(bpy.types.Operator):
     bl_idname = "object.sdf_ret_gen"
@@ -107,23 +106,36 @@ class SDFRetTexGenOperator(bpy.types.Operator):
     def execute(self, context):
         props = context.scene.SdfProperties
         
-        images = []
-        for tex in props.GeneratedTextures:
-            images.append(tex.image)
-        array = np.array(images[5].pixels[:], dtype=np.float32).reshape(images[5].size[1], images[5].size[0], 4)
+        # images = []
+        # for tex in props.GeneratedTextures:
+        #     images.append(tex.image)
+        
+        image = props.GeneratedTextures[0].image
+        array = np.array(image.pixels[:], dtype=np.float32).reshape(image.size[1], image.size[0], 4)
         data = array[:, :, 0].flatten()
         
-        #non_black_pixels = np.sum(data > 0)
-        #non_black_pixels = np.sum(np.any(array[:, :, :3] > 0, axis=-1))
-        #print(f"非黑色像素数量: {non_black_pixels}")
-        with concurrent.futures.process.ProcessPoolExecutor(max_workers = len(images)) as executor:
-            futures = []
-            for image in images:
-                array = np.array(image.pixels[:], dtype=np.float32).reshape(image.size[1], image.size[0], 4)
-                data = array[:, :, 0].flatten()
-                futures.append(executor.submit(ComputeSDF, data))
+        ret = ComputeSDF(data, image.size[0], image.size[1])
+        
+        for y in range(image.size[1]):
+            for x in range(image.size[0]):
+                index = (y * image.size[0] + x) * 4
+                val = ret[y * image.size[0] + x] / 255.0
+                image.pixels[index] = val
+                image.pixels[index + 1] = val
+                image.pixels[index + 2] = val
+                image.pixels[index + 3] = 1.0
+        image.update()
+
+        # with concurrent.futures.ThreadPoolExecutor(max_workers = len(images)) as executor:
+        #     futures = []
+        #     for image in images:
+        #         array = np.array(image.pixels[:], dtype=np.float32).reshape(image.size[1], image.size[0], 4)
+        #         data = array[:, :, 0].flatten()
+        #         futures.append(executor.submit(ComputeSDF, data, image.size[0], image.size[1]))
             
-            for future in concurrent.futures.as_completed(futures):
-                future.result()
+        #     for future in concurrent.futures.as_completed(futures):
+        #         temp = []
+        #         temp = future.result()
+        #         print(1)
         
         return {"FINISHED"}
