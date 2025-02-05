130ebe8c68a49d1874036dd42df59223e09ce6a2
thirtytwo <58972099+thirtytytwo@users.noreply.github.com>
2025-02-05 20:53:36 +0800

    gamma矫正

--- addons/addon_ComputeSdfFace/Shaders/CombineFrag.glsl
+++ addons/addon_ComputeSdfFace/Shaders/CombineFrag.glsl
@@ -1,10 +1,9 @@
 
 void main() {
-    float distA = texture(SDFA, uvInterp).r * 2.0 - 1.0;
+    float distA = texture(SDFA, uvInterp).r;
+    distA = pow(distA, 1.0 / 2.2);
     float distB = texture(SDFB, uvInterp).r * 2.0 - 1.0;
-    vec3 result = vec3(0,0,0);
-    if(distA < -0.5){
-        result = vec3(1,1,1);
-    }
-    FragColor = vec4(result, 1.0);
+    distB = pow(distB, 1.0 / 2.2);
+
+    FragColor = vec4(distB - distA, 0, 0, 1.0);
 }
