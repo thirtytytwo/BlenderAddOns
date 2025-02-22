1250a92414360e57aa1badcb803d94d2448fc71d
luyicheng <thirtytwo0301@gmail.com>
2025-02-22 16:20:01 +0800

    更新，完成增加顶点初步

--- addons/addon_ComputeOutlineNormal/operators/ModifyMeshVertex.py
+++ addons/addon_ComputeOutlineNormal/operators/ModifyMeshVertex.py
@@ -5,52 +5,46 @@ class ModifyMeshVertexOperator(bpy.types.Operator):
     bl_idname = "object.modifymeshvertex"
     bl_label = "ModifyMeshVertex"
 
-    def split_sharp_edge(self, bm, edge):
-        # 检查每个顶点的相连边是否有其他锐边
-        for vert in edge.verts:
-            sharpEdges = [e for e in vert.link_edges if not e.smooth and e != edge]
-            for sharpEdge in sharpEdges:
-                # 找到相连的面并删除
-                for face in vert.link_faces:
-                    if edge in face.edges and sharpEdge in face.edges:
-                        # 新的顶点
-                        result = bmesh.ops.split(bm, geom=[vert])
-                        newVert = result['geom'][0]
-                        newVert.co = vert.co
-                        newVert.normal = face.normal
-                        # 另外n个顶点
-                        verts = []
-                        for v in face.verts:
-                            if v == vert:
-                                verts.append(newVert)
-                            else:
-                                verts.append(v)
-                        if len(verts) > 4:
-                            self.report({'ERROR'}, "Face has more than 4 vertices")
-                            return {'CANCELLED'}
-                        # 组织新的面，由另外n个顶点和新的顶点组成
-                        newFace = bm.faces.new(verts)
-                        newFace.smooth = face.smooth    
-                        # 将新的面的边标记为平滑边
-                        for e in newFace.edges:
-                            e.smooth = True
-                        # 删除面
-                        bm.faces.remove(face)
-                        bm.faces.index_update()
-        # 将当前锐边标记为非锐边
-        edge.smooth = True
-
+    def split_sharp_edge(self, bm, face, edges):
+        # 新增的顶点容器
+        newVerts = []
+        # 根据两两相交锐边的相交点去增加顶点
+        for edge in edges:
+            for vert in edge.verts:
+                for e in vert.link_edges:
+                    if e in edges and e != edge:
+                        if vert.co not in [v.co for v in newVerts]:
+                            v = bm.verts.new(vert.co, vert)
+                            v.normal = face.normal
+                            newVerts.append(v)
+        
+        # 修改面
+        if newVerts is not None:
+            faceVerts = []
+            for vert in face.verts:
+                flag = False
+                for v in newVerts:
+                    if vert.co == v.co:
+                        flag = True
+                        faceVerts.append(v)
+                        break
+                if not flag:
+                    faceVerts.append(vert)
+            newFace = bm.faces.new(faceVerts)
+            newFace.smooth = face.smooth
+            bm.faces.remove(face)
         
     def execute(self, context):
         mesh = context.object.data
         bm = bmesh.new()
         bm.from_mesh(mesh)
         
-        for edge in bm.edges:
-            if not edge.smooth:
-                print(f"Edge{edge.index}is sharp.")
-                self.split_sharp_edge(bm, edge)
-        
+        for face in bm.faces:
+            sharpEdges = [e for e in face.edges if not e.smooth]
+            if len(sharpEdges) >= 2:
+                self.split_sharp_edge(bm, face,sharpEdges)
+            for edge in sharpEdges:
+                edge.smooth = True
         bm.to_mesh(mesh)
         mesh.update()   
         bm.free()
